09f71f7cd15b2672ecd245eba837b131dd655d74
MaelMIOSSEC <mama.miossec@gmail.com>
2026-03-27 20:08:04 +0100

    Update and Shortening src/pages/Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,3 +1,33 @@
+const priceCards = [
+  {
+    titre: "VISITE CHAT, CHIEN, NAC",
+    prix: "10 - 15 €",
+    duree: "30 minutes",
+    description: "Distribution de l'eau et de l'alimentation, soins, nettoyage de la litière/habitat de l'animal, jeux, brossage...",
+    couleurArrierePlan: "#ffcece",
+    couleurTexte: "#150e38",
+    couleurDelimitation: "black"
+  },
+  {
+    titre: "PROMENADE INDIVIDUELLE",
+    prix: "15 - 20 €",
+    duree: "30 minutes - 1 heure",
+    description: "Autour du domicile, à la campagne ou en forêt dans un endroit n'excédant pas plus de 10 minutes de trajet.",
+    couleurArrierePlan: "#5fb9a9",
+    couleurTexte: "white",
+    couleurDelimitation: "white"
+  },
+  {
+    titre: "VISITE POUR LES CHEVAUX",
+    prix: "SUR DEVIS",
+    duree: "Durée indéterminée",
+    description: "Un ou plusieurs passages par jour, sortie au pré. Distribution de l'eau et de l'alimentation, pansage, soins, box.",
+    couleurArrierePlan: "#ffffff",
+    couleurTexte: "#150e38",
+    couleurDelimitation: "black"
+  }
+];
+
 const StepCard = ({ number, text }: { number: string; text: string }) => (
   <div className="relative border p-6 rounded-4xl w-64 h-64 mt-10 bg-white shadow-lg">
     <h2 className="absolute -top-8 left-1/2 -translate-x-1/2 flex items-center justify-center w-14 h-14 rounded-full bg-white text-[#150e38] font-bold">
@@ -8,6 +38,26 @@ const StepCard = ({ number, text }: { number: string; text: string }) => (
       <p>{text}</p>
     </div>
   </div>
+);
+
+const PriceCard = () => (
+  (priceCards.map((priceCard) => (
+    <div
+    className="tarifs"
+    style={{ backgroundColor: priceCard.couleurArrierePlan, color: priceCard.couleurTexte }}
+    draggable="false"
+  >
+    <h5>
+      {priceCard.titre}
+    </h5>
+    <h2>{priceCard.prix}</h2>
+    <hr style={{ width: 100, border: "solid 1px", color: priceCard.couleurDelimitation }} />
+    <h5>{priceCard.duree}</h5>
+    <p>
+      {priceCard.description}
+    </p>
+  </div>
+  )))
 );
 
 export default function Services() {
@@ -34,62 +84,7 @@ export default function Services() {
           </div>
           <div className="test">
             <div className="service-img">
-              <div
-                className="tarifs"
-                style={{ backgroundColor: "#ffcece", color: "#150e38" }}
-                draggable="false"
-              >
-                <h5>VISITE CHAT, CHIEN, NAC</h5>
-                <h3>10 - 15 €</h3>
-                <hr style={{ width: 100, border: "solid 1px" }} />
-                <h5>30 minutes</h5>
-                <p>
-                  Distribution de l'eau et de l'alimentation, soins, nettoyage
-                  de la litière/habitat de l'animal, jeux, brossage...
-                </p>
-              </div>
-              <div className="service-img">
-                <div
-                  className="tarifs"
-                  style={{ backgroundColor: "#5fb9a9", color: "white" }}
-                  draggable="false"
-                >
-                  <h5 className="titre">PROMENADE INDIVIDUELLE</h5>
-                  <h3>15 - 20 €</h3>
-                  <hr
-                    style={{
-                      width: 100,
-                      border: "solid 1px",
-                      color: "white",
-                    }}
-                  />
-                  <h5>30 minutes - 1 heure</h5>
-                  <p>
-                    Autour du domicile, à la campagne ou en forêt dans un
-                    endroit n'excédant pas plus de 10 minutes de trajet.
-                  </p>
-                </div>
-              </div>
-              <div className="service-img">
-                <div
-                  className="tarifs"
-                  style={{
-                    backgroundColor: "#ffffff",
-                    color: "#150e38",
-                  }}
-                  draggable="false"
-                >
-                  <h5>VISITE POUR LES CHEVAUX</h5>
-                  <h3>SUR DEVIS</h3>
-                  <hr style={{ width: 100, border: "solid 1px" }} />
-                  <h5>Durée indéterminée</h5>
-                  <p>
-                    Un ou plusieurs passages par jour, sortie au pré.
-                    Distribution de l'eau et de l'alimentation, pansage, soins,
-                    box.
-                  </p>
-                </div>
-              </div>
+              <PriceCard />
             </div>
           </div>
         </div>
@@ -115,3 +110,4 @@ export default function Services() {
     </div>
   );
 }
+
